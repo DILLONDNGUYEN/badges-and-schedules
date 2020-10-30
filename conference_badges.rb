42e98038d093f4ccce75b55038da8f0d70345e3b
Dillon Nguyen <dillon.d.nguyen@gmail.com>
2020-10-29 23:18:55 -0700

    commit tree db6bbd66b24be897f56b4121b4ab08724e4c5217

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,32 @@
 # Write your code here.
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  newarray = []
+  array.each do |name|
+    newarray.push("Hello, my name is #{name}.")
+  end
+  return newarray
+end
+
+def assign_rooms(array)
+  newrooms = []
+  counter = 1
+  array.each do |name|
+    newrooms.push("Hello, #{name}! You'll be assigned to room #{counter}!")
+  counter += 1
+  end
+  return newrooms
+end
+
+def printer(array)
+  batch_badge_creator(array).each do |customer|
+    puts customer
+  end
+  assign_rooms(array).each do |customer|
+    puts customer
+  end
+end
